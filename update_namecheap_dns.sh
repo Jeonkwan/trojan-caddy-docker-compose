7de79c39b8103325e786ee94499056cbac5d06bf
Jeonkwan Chan <jeonkwan@gmail.com>
2022-03-27 01:33:52 +0800

    add script to update namecheap dns record

--- update_namecheap_dns.sh
+++ update_namecheap_dns.sh
@@ -10,4 +10,20 @@ read -r -p "Instance public IP address: " INSTANCE_PUBLIC_IP
 read -s -r -p "Namecheap Dynamice DNS Password: " NAMECHEAP_DDNS_PASS
 echo ""
 
-curl "https://dynamicdns.park-your-domain.com/update?host=${SUBDOMAIN}&domain=${DOMAIN}&password=${NAMECHEAP_DDNS_PASS}&ip=${INSTANCE_PUBLIC_IP}"
+function update_nc_dns_curl {
+    curl "https://dynamicdns.park-your-domain.com/update?host=${SUBDOMAIN}&domain=${DOMAIN}&password=${NAMECHEAP_DDNS_PASS}&ip=${INSTANCE_PUBLIC_IP}"
+}
+
+function update_nc_dns_docker {
+    docker run \
+    --name namecheap-ddns-update \
+    --rm \
+    -e "DOMAIN=${DOMAIN}" \
+    -e "SUBDOMAINS=${SUBDOMAIN}" \
+    -e "IP=${INSTANCE_PUBLIC_IP}" \
+    -e "NC_DDNS_PASS=${NAMECHEAP_DDNS_PASS}" \
+    joshuamorris3/namecheap-ddns-update
+}
+
+# by default using the docker method
+update_nc_dns_docker
