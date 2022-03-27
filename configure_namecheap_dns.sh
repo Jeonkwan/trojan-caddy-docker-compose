e57073f57ef181232cf0931a4c14e385c92c02a4
Jeonkwan Chan <jeonkwan@gmail.com>
2022-03-27 16:07:38 +0800

    add dedicated script for init namecheap dns record update

---
+++ configure_namecheap_dns.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+SAMPLE_DOMAIN="example.com"
+SAMPLE_SUBDOMAIN="sub1"
+read -r -p "Domain name(e.g.: ${SAMPLE_DOMAIN}): " DOMAIN
+read -r -p "Subdomain name(e.g.: ${SAMPLE_SUBDOMAIN}): " SUBDOMAIN
+read -r -p "Instance public IP address: " INSTANCE_PUBLIC_IP
+read -s -r -p "Namecheap Dynamice DNS Password: " NAMECHEAP_DDNS_PASS
+echo ""
+
+curl "https://dynamicdns.park-your-domain.com/update?host=${SUBDOMAIN}&domain=${DOMAIN}&password=${NAMECHEAP_DDNS_PASS}&ip=${INSTANCE_PUBLIC_IP}"
